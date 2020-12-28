e292bfc2bc6033be845d485f0de4bc8835dad31b
kazuma-fujita-genova <kazuma.fujita@genova.co.jp>
2020-12-28 18:36:42 +0900

    _app/_documentファイル作成

---
+++ src/pages/_app.tsx
@@ -0,0 +1,33 @@
+import React, { useEffect } from 'react';
+import { AppProps } from 'next/app';
+import { ThemeProvider as StyledComponentsThemeProvider } from 'styled-components';
+import { ThemeProvider as MaterialUIThemeProvider } from '@material-ui/core/styles';
+import { StylesProvider } from '@material-ui/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Layout from '../components/Layout';
+import theme from '../styles/theme';
+
+const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
+
+  useEffect(() => {
+    const jssStyles: any = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <StylesProvider injectFirst>
+      <MaterialUIThemeProvider theme={theme}>
+        <StyledComponentsThemeProvider theme={theme}>
+          <CssBaseline />
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </StyledComponentsThemeProvider>
+      </MaterialUIThemeProvider>
+    </StylesProvider>
+  );
+};
+
+export default MyApp;
