12cbd18be301f9fdc3df2b51c4c93b87dddc9750
kazuma-fujita-genova <kazuma.fujita@genova.co.jp>
2021-01-06 10:49:29 +0900

    不要ファイルの削除

--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -1,5 +1,5 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
-import Layout from '../../components/templates/Layout'
+import Layout from '../../components/templates/Layout';
 import { PostItem } from '../../interfaces/PostItem';
 import { POST_DETAIL } from '../../lib/constants';
 import { useGetAllPostIds, useGetPostData } from '../../state/PostDetail/hooks';
@@ -10,7 +10,7 @@ interface Props {
 }
 
 const Post: React.FC<Props> = ({ item, errors }) => (
-  <Layout title="ZUMA Tech Note" screenName={POST_DETAIL} item={item} />
+  <Layout title='ZUMA Tech Note' screenName={POST_DETAIL} item={item} />
 );
 
 export default Post;
@@ -19,16 +19,60 @@ export const getStaticPaths: GetStaticPaths = async () => {
   const paths = useGetAllPostIds();
   return {
     paths,
-    fallback: false
-  }
-}
+    fallback: false,
+  };
+};
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   // const postData = await useGetPostData(params.id);
   const item = useGetPostData(params?.id as string);
   return {
     props: {
-      item
-    }
-  }
-}
+      item,
+    },
+  };
+};
+
+// Error sample
+// type Props = {
+//   item?: User;
+//   errors?: string;
+// };
+
+// const StaticPropsDetail = ({ item, errors }: Props) => {
+//   if (errors) {
+//     return (
+//       <Layout title='Error | Next.js + TypeScript Example'>
+//         <p>
+//           <span style={{ color: 'red' }}>Error:</span> {errors}
+//         </p>
+//       </Layout>
+//     );
+//   }
+
+//   return (
+//     <Layout title={`${item ? item.name : 'User Detail'} | Next.js + TypeScript Example`}>
+//       {item && <ListDetail item={item} />}
+//     </Layout>
+//   );
+// };
+
+// export default StaticPropsDetail;
+
+// export const getStaticPaths: GetStaticPaths = async () => {
+//   const paths = sampleUserData.map((user) => ({
+//     params: { id: user.id.toString() },
+//   }));
+
+//   return { paths, fallback: false };
+// };
+
+// export const getStaticProps: GetStaticProps = async ({ params }) => {
+//   try {
+//     const id = params?.id;
+//     const item = sampleUserData.find((data) => data.id === Number(id));
+//     return { props: { item } };
+//   } catch (err) {
+//     return { props: { errors: err.message } };
+//   }
+// };
