3da59b4c88d7c601c8f24836cb9cbe689b79bbeb
kazuma-fujita-genova <kazuma.fujita@genova.co.jp>
2021-01-07 18:50:21 +0900

    global.css追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,8 @@ import { ThemeProvider as StyledComponentsThemeProvider } from 'styled-component
 import { ThemeProvider as MaterialUIThemeProvider } from '@material-ui/core/styles';
 import { StylesProvider } from '@material-ui/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import theme from '../styles/theme';
+import theme from 'styles/theme';
+import 'styles/global.css';
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => {
   useEffect(() => {
